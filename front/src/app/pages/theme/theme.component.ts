666b893ea0d0e9af4b1a0ab858734c347e875460
BEDDAR Rebiha <beddarrebiha@gmail.com>
2025-03-18 23:39:16 +0100

    s'abonner

--- front/src/app/pages/theme/theme.component.ts
+++ front/src/app/pages/theme/theme.component.ts
@@ -1,13 +1,11 @@
 import { Theme } from './../../interfaces/theme.interface';
 import { UserService } from 'src/app/services/user.service';
-import { Observable, map, of ,BehaviorSubject} from 'rxjs';
+import { Observable} from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from 'src/app/services/theme.service';
-import { Router } from '@angular/router';
 import { SessionService } from 'src/app/services/session.service';
 import { User } from 'src/app/interfaces/user.interface';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
 
 @Component({
   selector: 'app-home',
@@ -15,46 +13,46 @@ import { SessionInformation } from 'src/app/interfaces/sessionInformation.interf
   styleUrls: ['./theme.component.scss'],
 })
 export class ThemeListComponent implements OnInit {
+  public themes$: Observable<Theme[]> = this.themeService.all();
+  public userThemesSet = new Set<number>(); // Stocke les IDs des thèmes abonnés
 
-  public themes$: Observable<Theme[]> = this.themeService.all()
-  private abonnementSubject = new BehaviorSubject<boolean>(false);
   constructor(
     private themeService: ThemeService,
     private userService: UserService,
     private sessionService: SessionService,
     private matSnackBar: MatSnackBar
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    console.log(this.themes$)
-  }
-
-  abonner(theme: Theme) {
-    console.log(this.sessionService.sessionInformation?.id, theme)
-    let idUser = this.sessionService.sessionInformation?.id
-    this.userService.update(idUser, theme)
-      .subscribe((_: User) => this.matSnackBar.open('abonnement réussi', 'Close', { duration: 3000 }));
-    console.log(this.sessionService.sessionInformation)
-  }
-
-  isAbonne(theme: Theme): Observable<boolean> {
     const idUser = this.sessionService.sessionInformation?.id;
     if (idUser) {
       this.userService.getById(idUser.toString()).subscribe(user => {
-        const isSubscribed = user.themes.some(t => t.id === theme.id);
-        this.abonnementSubject.next(isSubscribed);
+        this.userThemesSet = new Set(user.themes.map(t => t.id)); // Stocke les IDs des thèmes
       });
-    } else {
-      console.error("Identifiant utilisateur non trouvé.");
-      this.abonnementSubject.next(false);
     }
-    return this.abonnementSubject.asObservable();
   }
 
-  desabonner(theme: Theme) {
-    let idUser = this.sessionService.sessionInformation?.id
-    this.userService.update(idUser, theme)
-      .subscribe((_: User) => this.matSnackBar.open('désabonnement réussi', 'Close', { duration: 3000 }));
+  checkAbonnement(theme: Theme): boolean {
+    return this.userThemesSet.has(theme.id);
   }
 
+  abonner(theme: Theme): void {
+    const idUser = this.sessionService.sessionInformation?.id;
+    if (!idUser) return;
+
+    this.userService.update(idUser, theme).subscribe((_: User) => {
+      this.userThemesSet.add(theme.id); // Ajoute immédiatement dans le Set
+      this.matSnackBar.open('Abonnement réussi', 'Fermer', { duration: 3000 });
+    });
+  }
+
+  desabonner(theme: Theme): void {
+    const idUser = this.sessionService.sessionInformation?.id;
+    if (!idUser) return;
+
+    this.userService.desabonner(idUser, theme).subscribe((_: User) => {
+      this.userThemesSet.delete(theme.id); // Retire immédiatement du Set
+      this.matSnackBar.open('Désabonnement réussi', 'Fermer', { duration: 3000 });
+    });
+  }
 }
